b3736cdac9b3f311fd4a27a3960a73d449d046b4
Kenichi Yonekawa <tcgrim@gmail.com>
2013-05-21 23:14:02 +0900

    Explode gorilla

--- app/layers/game_layer.rb
+++ app/layers/game_layer.rb
@@ -5,11 +5,23 @@ class GameLayer < Joybox::Core::Layer
     init_world
 
     on_touches_ended do |touches, event|
-      fire
+      touch = touches.anyObject
+      location = touch.locationInView(touch.view)
+      location = Director.sharedDirector.convertToGL(location)
+
+      fire(location)
     end
+
+    @destroyBodyQueue = []
 
     schedule_update do |delta|
       @world.step(delta: delta)
+
+      @destroyBodyQueue.each do |sprite|
+        @world.removeBody(sprite.body)
+        self.removeChild(sprite)
+      end
+      @destroyBodyQueue = []
     end
   end
 
@@ -32,24 +44,41 @@ class GameLayer < Joybox::Core::Layer
     end
   end
 
-  def fire
+  def fire(location)
     cannon_ball = create_cannon_ball
+    return unless cannon_ball
+
     self << cannon_ball
-    cannon_ball.body.apply_force(force: [6000, 100])
+    cannon_ball.body.apply_force(force: [300 * (location.x / 100), 300 * (location.y / 100)])
   end
 
   def create_cannon_ball
-    cannon_ball_body = @world.new_body(
-      position: [Screen.half_width, Screen.half_height],
+    return nil if @cannon_ball
+
+    @cannon_ball_body = @world.new_body(
+      position: [100, 100],
       type: KDynamicBodyType
     ) do
       polygon_fixture(
-        box: [56, 68],
+        box: [28, 34],
         friction: 0.3,
         density: 1.0
       )
     end
 
-    PhysicsSprite.new(file_name: 'gorilla.png', body: cannon_ball_body)
+    @cannon_ball = PhysicsSprite.new(file_name: 'gorilla.png', body: @cannon_ball_body)
+    @world.when_collide(@cannon_ball_body) do |collision_body, is_touching|
+      # App crashes by assertion error if removing body here
+      @destroyBodyQueue << @cannon_ball
+
+      explosion = CCParticleExplosion.node
+      explosion.position = @cannon_ball.position
+      self << explosion
+
+      self.removeChild(@cannon_ball)
+      @cannon_ball = nil
+    end
+
+    @cannon_ball
   end
 end
